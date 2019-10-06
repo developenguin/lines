84209faf1f388f856aeac7ee9abfe287c34f7fab
developenguin <developenguin@users.noreply.github.com>
2019-10-06 18:00:23 +0200

    Implement some basic coloring

--- index.js
+++ index.js
@@ -62,15 +62,23 @@ function getCanvasCenter() {
 
 function connectAllPoints(points) {
 
-  ctx.strokeStyle = 'white';
+  const colors = getColorsForN(points.length);
+  ctx.strokeStyle = `rgba(255, 255, 255, ${1/(points.length/10)})`;
 
-  points.forEach(point => {
 
-    points.forEach(otherPoint => {
+
+  points.forEach((point, idx) => {
+
+    const color = colors[idx % colors.length];
+    ctx.strokeStyle = `hsla(${color.h}, ${color.s * 100}%, ${color.l * 100}%)`;
+
+    points.forEach((otherPoint, otherIdx) => {
+
       ctx.beginPath();
       ctx.moveTo(point.x, point.y);
       ctx.lineTo(otherPoint.x, otherPoint.y);
       ctx.stroke();
+
     });
 
   });
@@ -81,20 +89,41 @@ function getPointsForN(n) {
 
   const points = [],
         center = getCanvasCenter(),
-        segmentAngle = 2 * Math.PI / n,
-        circleRadius = canvas.width > canvas.height
-          ? canvas.height / 2.5
-          : canvas.width / 2.5;
+        segmentAngleRadian = 2 * Math.PI / n,
+        circleRadius = getCircleRadius();
 
   for (let i = 1; i <= n; i++) {
     points.push({
-      x: center.x + circleRadius * Math.cos(i * segmentAngle),
-      y: center.y + circleRadius * Math.sin(i * segmentAngle)
+      x: center.x + circleRadius * Math.cos(i * segmentAngleRadian),
+      y: center.y + circleRadius * Math.sin(i * segmentAngleRadian)
     });
   }
 
   return points;
 
+}
+
+function getColorsForN(n) {
+
+  const segmentAngleDegrees = 2 * Math.PI / n * 180 / Math.PI,
+        colors = [];
+
+  for (let i = 1; i <= n; i++) {
+    colors.push({
+      h: i * segmentAngleDegrees,
+      s: 100 / i,
+      l: 0.5
+    });
+  }
+
+  return colors;
+
+}
+
+function getCircleRadius() {
+  return canvas.width > canvas.height
+    ? canvas.height / 2.5
+    : canvas.width / 2.5;
 }
 
 window.onload = () => init();
